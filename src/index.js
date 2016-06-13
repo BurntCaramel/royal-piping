929417c3275a623328c005c8315892d22b6231de
Patrick Smith <pgwsmith@gmail.com>
2016-06-13 15:55:11 +1000

    Add key to rendered content items

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import Wrapper from './Wrapper'
 export default function RoyalPiping({ content = [], deviceInfo = {} }) {
 	return (
 		<Wrapper { ...deviceInfo }>{
-			[].concat(content).map(({ type, ...props }) => {
+			[].concat(content).map(({ type, ...props }, index) => {
 				const Component = typeToComponent[type]
-				return <Component { ...props} />
+				return <Component key={ index } { ...props} />
 			})
 		}</Wrapper>
 	)
