150eaca5b6ae219c84fe6e9ea18b43fe76bb020c
Patrick Smith <pgwsmith@gmail.com>
2016-06-13 15:50:39 +1000

    Remove now ignored files

--- es6/stylers/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export { default as boxSizing } from './boxSizing';
-export { default as flex } from './flex';
-export { default as margin } from './margin';
-export { default as padding } from './padding';
-export { default as transition } from './transition';
-
-export { default as dark } from './dark';
