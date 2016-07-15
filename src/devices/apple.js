74bb9d8f2e35914cf763816a4c8be2baf877811e
Patrick Smith <pgwsmith@gmail.com>
2016-07-15 22:34:11 +1000

    Add present Apple device specs

---
+++ src/devices/apple.js
@@ -0,0 +1,5 @@
+export default {
+	"iphone5": { title: 'iPhone 5', width: 320, height: 568 },
+	"iphone6": { title: 'iPhone 6', width: 375, height: 667 },
+	"iphone6plus": { title: 'iPhone 6 Plus', width: 414, height: 736 }
+}
