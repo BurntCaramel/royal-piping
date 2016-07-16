0dbf9032a02476c80dbb5d876df32fdac4081445
Patrick Smith <pgwsmith@gmail.com>
2016-07-16 11:11:33 +1000

    Add big Button variation

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,14 @@ import React from 'react'
 import Box from './Box'
 import Text from './Text'
 
-export default function Button({ title, enabled = true, align, size, fontWeight, border = true, ...boxProps }) {
+export default function Button({ title, enabled = true, align = 'center', size, fontWeight, border = true, big = false, ...boxProps }) {
+	const extraProps = {}
+	if (big) {
+		extraProps.padding = 20
+	}
+
 	return (
-		<Box border={ border } { ...boxProps }>
+		<Box border={ border } { ...extraProps } { ...boxProps }>
 			<Text enabled={ enabled } align={ align } size={ size } fontWeight={ fontWeight }>
 				{ title }
 			</Text>
