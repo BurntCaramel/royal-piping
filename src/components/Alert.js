cd04c0d8d58010a4cfecb7cef041096b8cb6a594
Patrick Smith <pgwsmith@gmail.com>
2016-07-16 11:11:13 +1000

    Add textFields to Alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -4,16 +4,47 @@ import Box from './Box'
 import Text from './Text'
 import Button from './Button'
 
-export default function Alert({ title, message, actions }) {
+export default function Alert({ title, message, textFields, actions }) {
+	const actionCount = actions.length
+	const buttonFlexBasis = (actionCount <= 2) ? `${ 1.0 / actionCount }%` : 'auto'
 	const buttonElements = actions.map(({
 		title,
 		enabled = true,
 		preferred = false
 	}, index) => (
 		<Button key={ index }
-			title={ title } enabled={ enabled } fontWeight={ preferred ? 'bold' : 'normal' } grow={ 1 } align='center' padding={ 8 }
+			title={ title }
+			enabled={ enabled }
+			fontWeight={ preferred ? 'bold' : 'normal' }
+			grow={ 1 }
+			basis={ buttonFlexBasis }
+			align='center'
+			padding={ 8 }
 		/>
 	))
+
+	const buttonsBox = (actionCount <= 2) ? (
+		<Box row shrink={ 0 } children={ buttonElements } />
+	) : (
+		<Box column shrink={ 0 } children={ buttonElements } />
+	)
+
+	let textFieldsBox;
+	if (textFields && textFields.length > 0) {
+		const textFieldElements = textFields.map(({
+			title
+		}, index) => (
+			<Box key={ index }
+				border
+				padding={ 6 }
+			>
+				<Text size={ 13 }>
+					{ title }
+				</Text>
+			</Box>
+		))
+		textFieldsBox = <Box column shrink={ 0 } margin={{ top: 16 }} children={ textFieldElements } />
+	}
 
 	return (
 		<Box row grow={ 1 } alignItems='center'>
@@ -25,8 +56,9 @@ export default function Alert({ title, message, actions }) {
 					<Text align='center' size={ 13 } lineHeight={ 1.3 }>
 						{ message }
 					</Text>
+					{ textFieldsBox }
 				</Box>
-				<Box row shrink={ 0 } children={ buttonElements } />
+				{ buttonsBox }
 			</Box>
 		</Box>
 	)
