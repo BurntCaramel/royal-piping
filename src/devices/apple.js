d32ceee91bdc8cf5298560bd9de33145b47b8699
Patrick Smith <pgwsmith@gmail.com>
2016-07-16 11:10:47 +1000

    Adjust apple devices exports

--- src/devices/apple.js
+++ src/devices/apple.js
@@ -1,5 +1,25 @@
-export default {
-	"iphone5": { title: 'iPhone 5', width: 320, height: 568 },
-	"iphone6": { title: 'iPhone 6', width: 375, height: 667 },
-	"iphone6plus": { title: 'iPhone 6 Plus', width: 414, height: 736 }
+export const iphone5 = {
+	title: 'iPhone 5',
+	width: 320,
+	height: 568
+}
+
+export const iphone6 = {
+	title: 'iPhone 6',
+	width: 375,
+	height: 667
+}
+
+export const iphone6plus = {
+	title: 'iPhone 6 Plus',
+	width: 414,
+	height: 736
+}
+
+export function makeMap() {
+	return new Map([
+		["iphone5", iphone5],
+		["iphone6", iphone6],
+		["iphone6plus", iphone6plus]
+	])
 }
