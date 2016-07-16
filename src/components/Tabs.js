6c315ae068246166f26cd8b20fa136915623098b
Patrick Smith <pgwsmith@gmail.com>
2016-07-16 13:15:08 +1000

    Adjust iOS tabs

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -6,7 +6,7 @@ const height = 49
 
 export default function Tab({ items, showIcons = true }) {
 	return (
-		<Box row shrink={ 0 }>
+		<Box row outline shrink={ 0 }>
 			{
 				items.map((item, index) => (
 					<TabItem key={ index } { ...item } showIcon={ showIcons } />
@@ -16,10 +16,10 @@ export default function Tab({ items, showIcons = true }) {
 	)
 }
 
-const iconInset = 4
+const iconInset = 5
 
 const Icon = ({ selected }) => (
-	<Box margin={ iconInset } width={ 30 - (iconInset * 2) } height={ 30 - (iconInset * 2) } outline={ !selected } backgroundColor={ selected ? '#555' : null } />
+	<Box margin={ iconInset } width={ 34 - (iconInset * 2) } height={ 34 - (iconInset * 2) } outline={ !selected } backgroundColor={ selected ? '#555' : null } cornerRadius='50%' />
 )
 
 const selectedIcon = Icon({ selected: true })
@@ -27,11 +27,11 @@ const nonselectedIcon = Icon({ selected: false })
 
 function TabItem({ title, url, selected, showIcon }) {
 	return (
-		<Box outline url={ url } column alignItems='center' grow={ 1 } height={ height }>
+		<Box url={ url } column alignItems='center' grow={ 1 } height={ height }>
 			{ showIcon &&
 				(selected ? selectedIcon : nonselectedIcon)
 			}
-			<Text align='center' size={ 13 }>
+			<Text align='center' size={ 10 }>
 				{ title }
 			</Text>
 		</Box>
